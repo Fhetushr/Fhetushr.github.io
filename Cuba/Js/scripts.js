0cca3a25bf5bb2fd6a4675692b8bd45b17b7df3a
Fhetushr <69198472+Fhetushr@users.noreply.github.com>
2022-11-29 20:43:26 +0300

    Delete Cuba directory

--- Cuba/Js/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $(".header__burger").click(function(event) {
-		$(".header__list").toggleClass("active");
-		$(".header__burger").toggleClass("active");
-		$(".header__menu").toggleClass("active");
-		$("body").toggleClass("active");
-	});
-});
